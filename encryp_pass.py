53cbbe8c08fff6decbc4d3bacb4df214d431f690
Oscar <Oscar@oscar.com>
2018-03-07 08:21:02 +0100

    Version 1.3

--- encryp_pass.py
+++ encryp_pass.py
@@ -3,7 +3,6 @@ from passlib.hash import pbkdf2_sha256
 import sys
 
 
-
 def getParams(param1, param2, param3, param4):
     global password
     password = param1
@@ -17,42 +16,31 @@ def getParams(param1, param2, param3, param4):
 
 
     if (mode == '256' and action == 'validate') :
-        return validate_256()
+        global password
+        global password_hash
+
+        return pbkdf2_sha256.verify(password, password_hash)
+
+
     if (mode == '256' and action == 'create') :
-        return create_256()
+        global password
+
+        return pbkdf2_sha256.encrypt(password, rounds=200000, salt_size=16)
+
+
     if (mode == '512' and action == 'validate') :
-        return validate_512()
+        global password
+        global password_hash
+
+        return pbkdf2_sha512.verify(password, password_hash)
+
+
     if (mode == '512' and action == 'create') :
-        return create_512()
+        global password
+
+        return pbkdf2_sha512.encrypt(password, rounds=200000, salt_size=16)
 
 
-def validate_256():
-    global password
-    global password_hash
-
-    return pbkdf2_sha256.verify(password, password_hash)
-
-
-
-def validate_512():
-    global password
-    global password_hash
-
-    return pbkdf2_sha512.verify(password, password_hash)
-
-
-
-def create_256():
-    global password
-
-    return pbkdf2_sha256.encrypt(password, rounds=200000, salt_size=16)
-
-
-
-def create_512():
-    global password
-
-    return pbkdf2_sha512.encrypt(password, rounds=200000, salt_size=16)
 
 
 # sys.argv[2] = password
@@ -62,9 +50,10 @@ def create_512():
 
 if __name__ == "__main__":
 
+
     arguments = len(sys.argv)
 
     if arguments >= 5:
-        getParams(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+        print(getParams(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]))
     else:
         print("Number of arguments invalid")
